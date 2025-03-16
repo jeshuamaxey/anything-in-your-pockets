326b676ac691b818eb8985802085abc3aec73c39
Jeshua Maxey <me@jeshua.co>
2025-03-16 16:39:05 +0000

    bag alert styling is waaaaaaay better

--- src/components/game/common/BagLabel.tsx
+++ src/components/game/common/BagLabel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Bag } from '@/types/gameTypes';
 import LabelProgressBar from './LabelProgressBar';
 
@@ -41,14 +41,25 @@ export const BagLabel = ({
     && alerts.suspiciousItemAlert ? bag.suspicious_item_dealt_with : true
     && alerts.electronicsAlert ? bag.electronics_alert_dealt_with : true
     && alerts.liquidsAlert ? bag.liquids_alert_dealt_with : true
+  
+  const bgAnimation = useMemo(() => {
+    let bgAnimation = ""
+    if(alerts.electronicsAlert || alerts.liquidsAlert) {
+      bgAnimation = "animate-flash-bg-yellow border border-yellow-500"
+    }
+    if(alerts.suspiciousItemAlert) {
+      bgAnimation = "animate-flash-bg-red border border-red-500"
+    }
+    if(showAlerts && anyAlert && allAlertsDealtWith) {
+      bgAnimation = "border border-green-500"
+    }
+
+    return bgAnimation
+  }, [showAlerts, alerts.electronicsAlert, alerts.liquidsAlert, alerts.suspiciousItemAlert, allAlertsDealtWith])
 
   return (
-    <div className="bg-white rounded w-full"
-      onClick={handleClick}
-      >
-      <div 
-        className={`w-full text-[11px] flex items-center justify-between gap-1 px-1 ${onClick ? 'cursor-pointer hover:bg-gray-50' : ''}`}
-      >
+    <div className={`relative bg-background ${bgAnimation} rounded w-full overflow-visible`} onClick={handleClick}>
+      <div className={`w-full text-[11px] flex items-center justify-between gap-1 px-1 ${onClick ? 'cursor-pointer hover:bg-gray-50' : ''}`}>
         <div className="flex flex-1 items-center gap-2">
           <span className="font-mono">💼 {bag.id.slice(-3)}</span>
           {showAlerts && alerts.suspiciousItemAlert && !bag.suspicious_item_dealt_with && <span className="text-xs text-red-500">🚨</span>}
@@ -59,6 +70,8 @@ export const BagLabel = ({
         {showProgress && <LabelProgressBar progress={progress} color="blue" />}
         {annotation && <span className="text-xs text-gray-500">{annotation}</span>}
       </div>
+      {/* top letf */}
+      {/* <div className={`absolute rounded-full size-2 -top-1 -end-1 z-50 ${animation}`}></div> */}
     </div>
   );
 };
